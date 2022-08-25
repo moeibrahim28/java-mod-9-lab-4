bd32e1ca45a9d7e4908022c7513944ace1070c47
Moe <m.ibrahim0228@gmail.com>
2022-08-25 16:29:56 -0400

    setup for lab 4 done

--- flatiron-ng-messenger/cypress/e2e/spec.cy.ts
+++ flatiron-ng-messenger/cypress/e2e/spec.cy.ts
@@ -8,11 +8,18 @@ describe("empty spec", () => {
     cy.contains("New Message").click();
     cy.url().should("include", "contactList");
 
-    cy.contains("Start Message").click();
-    cy.url().should("equal", appURL);
+  }
+  );
 
-    cy.get("input[name=user-message-input]").type(testMessage); // get the input box by it 'name' attribute and type in it
-    cy.contains("Send").click(); // "Send" the message
-    cy.get("app-conversation-thread-component").should("contain", testMessage); // check that the message was sent
+  it("passes", () => {
+    cy.visit(appURL+"contactList");
+    cy.contains("Start Message").click();
+    cy.url().should("equal", appURL+"1");
+
+
+    //   cy.get("input[name=user-message-input]").type(testMessage); // get the input box by it 'name' attribute and type in it
+    // cy.contains("Send").click(); // "Send" the message
+    // cy.get("app-conversation-thread-component").should("contain", testMessage); // check that the message was sent
+  
   });
 });
